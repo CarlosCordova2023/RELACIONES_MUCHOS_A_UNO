c813ea7684bd9077d303143da3358a4ea6d0edb3
CarlosCordova2023 <learningcommun@gmail.com>
2024-10-02 20:17:32 -0300

    Add untracked files

---
+++ fabrica/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Fabricante(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField()
+    fabricante = models.ForeignKey(Fabricante, on_delete=models.CASCADE, related_name='productos')
+
+    def __str__(self):
+        return self.nombre
---
+++ productos/urls.py
@@ -0,0 +1,10 @@
+# productos/urls.py
+
+from django.urls import path
+from .views import lista_productos, agregar_producto
+
+urlpatterns = [
+    path('productos/', lista_productos, name='lista_productos'),
+    path('', lista_productos, name='lista_productos'),
+    path('productos/agregar/', agregar_producto, name='agregar_producto'),
+]
